b1cb2bfa945353426e5888fd0e0089cdf8927034
Emiliano Perez <emi.perez997@gmail.com>
2024-02-07 16:52:57 -0300

    [ADD] v1.0 Finish

--- src/utils/styles.js
+++ src/utils/styles.js
@@ -2,31 +2,36 @@ export const tagBg = {
   astro: {
     bg: "bg-blue-700 dark:bg-blue-800",
     hover: "hover:bg-blue-800 dark:hover:bg-blue-900",
-    text: "text-blue-100 dark:text-blue-800",
+    text: "text-blue-100 dark:text-blue-800 hover:text-blue-200 dark:hover:text-blue-100",
   },
   tutorial: {
     bg: "bg-green-600 dark:bg-green-800",
     hover: "hover:bg-green-700 dark:hover:bg-green-900",
-    text: "text-green-100 dark:text-green-800",
+    text: "text-green-100 dark:text-green-800 hover:text-green-200 dark:hover:text-green-100",
   },
   npm: {
     bg: "bg-red-600 dark:bg-red-800",
     hover: "hover:bg-red-700 dark:hover:bg-red-900",
-    text: "text-red-100 dark:text-red-800",
+    text: "text-red-100 dark:text-red-800 hover:text-red-200 dark:hover:text-red-100",
   },
   javascript: {
     bg: "bg-yellow-600 dark:bg-yellow-800",
     hover: "hover:bg-yellow-700 dark:hover:bg-yellow-900",
-    text: "text-yellow-100 dark:text-yellow-900",
+    text: "text-yellow-100 dark:text-yellow-900 hover:text-yellow-200 dark:hover:text-yellow-800",
   },
   tailwindcss: {
     bg: "bg-indigo-600 dark:bg-indigo-800",
     hover: "hover:bg-indigo-700 dark:hover:bg-indigo-900",
-    text: "text-indigo-100 dark:text-indigo-700",
+    text: "text-indigo-100 dark:text-indigo-700 hover:text-indigo-200 dark:hover:text-indigo-100",
   },
   react: {
     bg: "bg-blue-600 dark:bg-blue-800",
     hover: "hover:bg-blue-700 dark:hover:bg-blue-900",
-    text: "text-blue-100 dark:text-blue-800",
+    text: "text-blue-100 dark:text-blue-800 hover:text-blue-200 dark:hover:text-blue-100",
+  },
+  csharp: {
+    bg: "bg-purple-600 dark:bg-purple-800",
+    hover: "hover:bg-purple-700 dark:hover:bg-purple-900",
+    text: "text-purple-100 dark:text-purple-800 hover:text-purple-200 dark:hover:text-purple-100",
   },
 };
