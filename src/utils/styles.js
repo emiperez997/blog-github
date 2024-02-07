cbeb0d8eb270142dfd2e33f60d898803125074cb
Emiliano Perez <emi.perez997@gmail.com>
2024-02-07 12:39:20 -0300

    [ADD] Fix tag text

--- src/utils/styles.js
+++ src/utils/styles.js
@@ -2,31 +2,31 @@ export const tagBg = {
   astro: {
     bg: "bg-blue-700 dark:bg-blue-800",
     hover: "hover:bg-blue-800 dark:hover:bg-blue-900",
-    text: "text-blue-100 dark:text-blue-300",
+    text: "text-blue-100 dark:text-blue-800",
   },
   tutorial: {
     bg: "bg-green-600 dark:bg-green-800",
     hover: "hover:bg-green-700 dark:hover:bg-green-900",
-    text: "text-green-100 dark:text-green-300",
+    text: "text-green-100 dark:text-green-800",
   },
   npm: {
     bg: "bg-red-600 dark:bg-red-800",
     hover: "hover:bg-red-700 dark:hover:bg-red-900",
-    text: "text-red-100 dark:text-red-300",
+    text: "text-red-100 dark:text-red-800",
   },
   javascript: {
     bg: "bg-yellow-600 dark:bg-yellow-800",
     hover: "hover:bg-yellow-700 dark:hover:bg-yellow-900",
-    text: "text-yellow-100 dark:text-yellow-300",
+    text: "text-yellow-100 dark:text-yellow-900",
   },
   tailwindcss: {
     bg: "bg-indigo-600 dark:bg-indigo-800",
     hover: "hover:bg-indigo-700 dark:hover:bg-indigo-900",
-    text: "text-indigo-100 dark:text-indigo-300",
+    text: "text-indigo-100 dark:text-indigo-700",
   },
   react: {
     bg: "bg-blue-600 dark:bg-blue-800",
     hover: "hover:bg-blue-700 dark:hover:bg-blue-900",
-    text: "text-blue-100 dark:text-blue-300",
+    text: "text-blue-100 dark:text-blue-800",
   },
 };
